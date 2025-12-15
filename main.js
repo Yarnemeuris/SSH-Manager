0110df25740043adc051b5b15ea0a8dd9ec80d3d
Yarnemeuris <yarne.meuris@gmail.com>
2025-12-15 12:40:25 +0100

    export

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron')
 
-const path = require('node:path')
-const fs = require('node:fs')
+const { join } = require('node:path')
+const { existsSync, writeFileSync, readFileSync } = require('node:fs')
 const { spawn } = require('node:child_process')
 
 var config = { hosts: {} }
@@ -11,24 +11,16 @@ const createWindow = () => {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: join(__dirname, 'preload.js')
         }
     })
-
-    fs.watch(path.join(__dirname, "frontEnd"), (event) => {
-        if (event != "change") return
-
-        win.webContents.send("reload")
-    })
-
-    globalShortcut.register("F11", () => { win.webContents.toggleDevTools() })
 
     win.loadFile('frontEnd/index.html')
     win.removeMenu()
 }
 
 function save() {
-    fs.writeFileSync("./config.json", JSON.stringify(config));
+    writeFileSync("./config.json", JSON.stringify(config));
 }
 
 app.whenReady().then(() => {
@@ -41,11 +33,11 @@ app.whenReady().then(() => {
         var host = config.hosts[name]
 
         if (host.password == "") {
-            spawn("ssh", [`${host.user}@${host.IP}`], { shell: true, detached: true });
+            spawn("ssh", [`${host.user}@${host.IP} -p ${host.port}`], { shell: true, detached: true });
             return;
         }
 
-        spawn("wsl", [`sshpass -p ${host.password} ssh ${host.user}@${host.IP}`], { shell: true, detached: true });
+        spawn("wsl", [`sshpass -p ${host.password} ssh ${host.user}@${host.IP} -p ${host.port}`], { shell: true, detached: true });
     })
     createWindow()
 })
@@ -54,6 +46,6 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
 
-if (fs.existsSync("./config.json")) {
-    config = JSON.parse(fs.readFileSync("./config.json", { encoding: "utf8" }))
+if (existsSync("./config.json")) {
+    config = JSON.parse(readFileSync("./config.json", { encoding: "utf8" }))
 } else console.log("no config");
